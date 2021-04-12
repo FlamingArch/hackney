ee3a9cbb5d2bb15e6e400a3899521de52fe58e58
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-04-12 22:16:13 +0530

    Initial Commit - Added Project Files

---
+++ Shared/hackneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackneyApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct hackneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
