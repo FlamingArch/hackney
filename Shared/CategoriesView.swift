51bd4f75ddc82f0e9e44f1736ad9747ccbdc288b
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-13 14:36:18 +0530

    Sidebar More Section

--- Shared/CategoriesView.swift
+++ Shared/CategoriesView.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Harsh Chaturvedi on 12/04/21.
 //
-
 import SwiftUI
-
 struct CategoriesView: View {
     var categories = [["newspaper", "Posts"], ["questionmark.circle","AskHN"], ["eye.fill","Show"], ["briefcase","Jobs"]]
     #if os(macOS)
@@ -14,27 +12,29 @@ struct CategoriesView: View {
     #else
     private let listStyle = InsetGroupedListStyle()
     #endif
-    
+        
     var body: some View {
         NavigationView {
             List {
                 Section(header: Text("Hacker News")) {
                     ForEach(0..<categories.count) { index in
-                        NavigationLink(destination: Text("Sup")) {
+                        NavigationLink(destination: Text("Sup").navigationTitle(categories[index][1])) {
                             Image(systemName: categories[index][0])
                                 .foregroundColor(.accentColor)
                             Text(categories[index][1])
                         }
                     }
                 }
-                
+                Section(header: Text("More")) {
+                    NavigationLink(destination: Text("Sup").navigationTitle("Saved")) {
+                        Image(systemName: "newspaper")
+                        Text("Saved")
+                    }
+                }
             }
             .listStyle(listStyle)
             .navigationTitle("Hackney")
         }
     }
 }
-
-
-//
 
