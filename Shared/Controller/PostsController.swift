5c505509a40325d65aa72ca12f50ee771dc6e29c
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-07-29 23:49:02 +0530

    Fetch and Display Top Posts in PostsView

--- Shared/Controller/PostsController.swift
+++ Shared/Controller/PostsController.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 class PostsController: ObservableObject {
+    
+    struct TopPosts {
+        var posts: [Int]
+    }
+    
     @Published var posts: [HNItem] = []
     
-    init() { getPosts() }
+    init() { PostsController.getTopStoriesIDs { self.getPosts(ids: $0) } }
+    
+    static func fetchJSON(_ url: URL?, completion: @escaping (Data?, Error?) -> Void ) {
+        guard let url = url else {
+            fatalError("Invalid URL")
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                if error != nil { return completion(nil, error) }
+                
+                let response = (response as? HTTPURLResponse)?.statusCode
+                guard let response = response else {
+                    fatalError("Response Error")
+                }
+                guard response == 200 else {
+                    fatalError("Response Code Error")
+                }
+                
+                if let data = data {
+                    completion(data, nil)
+                }
+            }
+        }.resume()
+    }
     
     static func fetchJSON(_ id: Int, completion: @escaping (Data?, Error?) -> Void ) {
         guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(id).json") else {
@@ -36,20 +67,19 @@ class PostsController: ObservableObject {
                 }
             }
         }.resume()
-        
     }
     
-    static func decodeJSON(data: Data) -> HNItem {
-        if let decoded = try? JSONDecoder().decode(HNItem.self, from: data) {
+    static func decodeJSON <T: Decodable> (data: Data) -> T {
+        if let decoded = try? JSONDecoder().decode(T.self, from: data) {
             return decoded
         } else {
             fatalError("DecodeError")
         }
     }
     
-    func getPosts(count: Int = 20) {
-        for _ in 0..<count {
-            PostsController.fetchJSON(8863) { data, error in
+    func getPosts(ids: [Int]) {
+        for id in ids {
+            PostsController.fetchJSON(id) { data, error in
                 if error != nil {
                     print(error!)
                     return
@@ -59,6 +89,21 @@ class PostsController: ObservableObject {
                 } else { print("Nothing Received") }
             }
         }
+    }
+    
+    static func getTopStoriesIDs(completion: @escaping ([Int])->Void) {
+        
+        fetchJSON(URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")) { data, error in
+            if error != nil {
+                print(error!)
+                return
+            }
+            if let data = data {
+                completion(decodeJSON(data: data))
+            }
+        }
+
+//        completion([ 27998878, 27996684, 27996321, 27997167, 27997923, 27998323, 27998058, 27998642, 27998439, 27971019, 27998250, 27998109, 27996057, 27995788, 27998708, 27998282, 27993882, 27998958, 27980463, 27996130, 27996348, 27970847, 27992500, 27995057, 27985167, 27969978, 27971454, 27994866, 27994369, 27994092, 27994194, 27994692, 27991322, 27999112, 27993564, 27995313, 27996237, 27998837, 27996703, 27996693, 27993850, 27994480, 27994914, 27971255, 27997613, 27995130, 27992073, 27997599, 27997224, 27998732, 27992953, 27991120, 27970113, 27998648 ])
     }
     
 }
--- Shared/View/PostsView.swift
+++ Shared/View/PostsView.swift
@@ -16,14 +16,17 @@ struct PostsView: View {
         List {
             ForEach(controller.posts, id: \.id) { post in
                 NavigationLink(
-                    destination: WebView(request: URLRequest(url: URL(string: post.url!)!))
-                        .navigationTitle(post.url!)
-                        .navigationBarTitleDisplayMode(.inline)
+                    destination:
+                        // TODO: Safely Check and Unwrap URL, and show WebView Accordingly
+                        // WebView(request: URLRequest(url: URL(string: post.url ?? "https://google.com")!))
+                        // .navigationTitle(post.url!)
+                        // .navigationBarTitleDisplayMode(.inline)
+                        Text("Sup")
                 ) {
                     VStack(alignment: .leading) {
                         Text(post.title!).font(.headline).lineLimit(1)
                         Text(post.by!).font(.subheadline).lineLimit(1)
-                        //                        Text(samplePost.text!).font(.subheadline).lineLimit(2).opacity(0.5)
+                        // Text(samplePost.text!).font(.subheadline).lineLimit(2).opacity(0.5)
                     }
                 }
             }
