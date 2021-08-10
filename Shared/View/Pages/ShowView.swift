b2177d13ea56d89c6142d69a17ee5e57733ca9ed
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-08-10 14:54:18 +0530

    Added Show Page

--- Shared/View/Pages/ShowView.swift
+++ Shared/View/Pages/ShowView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 struct ShowView: View {
     @ObservedObject var controller: PostsController
     var body: some View {
-        Text("Work Under Progress!")
+        List {
+            if (controller.ask.count < 20) { ProgressView() }
+            ForEach(controller.show, id: \.id) { post in
+                NavigationLink(
+                    destination:
+                        post.url != nil ?
+                        WebView(request: URLRequest(url: URL(string: post.url!)!))
+                        .navigationBarTitle(post.url!, displayMode: .inline)
+                        : WebView(request: URLRequest(url: URL(string: "https://news.ycombinator.com")!))
+                        .navigationBarTitle("https://news.ycombinator.com", displayMode: .inline)
+                ) {
+                    VStack(alignment: .leading) {
+                        Text(post.title!).font(.headline).lineLimit(3)
+                        if let unwrapped = post.text {
+                            Text(unwrapped).lineLimit(3)
+                        }
+                        HStack {
+                            if let unwrapped = post.score {
+                                Text(String(unwrapped) + " Points").font(.subheadline).lineLimit(1)
+                            }
+                            if let unwrapped = post.descendants {
+                                Text(String(unwrapped) + " Comments").font(.subheadline).lineLimit(1)
+                            }
+                        }
+                        Text("By " + post.by!).font(.subheadline).lineLimit(1).foregroundColor(.secondary)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Show")
+        .onAppear { controller.getShowPosts() }
     }
 }
-
