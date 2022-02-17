0cc9e62e54975253f5b9542dd08569cc0150a224
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-02-17 22:02:07 +0530

    Created new Project

---
+++ Shared/HackneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackneyApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HackneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
