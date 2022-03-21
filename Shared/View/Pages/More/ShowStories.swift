1f2ed877495e26989b30d718cd414d3817aecea0
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-03-21 13:54:09 +0530

    Fixed Show Stories Screen

--- Shared/View/Pages/More/ShowStories.swift
+++ Shared/View/Pages/More/ShowStories.swift
@@ -12,33 +12,31 @@ struct ShowStories: View {
     @EnvironmentObject var viewModel: HackneyViewModel
     
     var body: some View {
-        Text("Sup")
-            .toolbar {
-                Group {
-                    if (viewModel.showStories.count < 12) {
-                        ProgressView()
-                    } else {
-                        List {
-                            ForEach(viewModel.showStories) { item in
-                                NavigationLink {
-                                    PostScreen(item: item)
-                                } label: {
-                                    PostListItem(item: item)
-                                }
-                            }
-                        }
-                    }
-                }
-                .toolbar {
-                    ToolbarItem(placement: .primaryAction) {
-                        Button {
-                            // TODO: Implement Hide Viewed Posts
+        Group {
+            if (viewModel.showStories.count < 12) {
+                ProgressView()
+            } else {
+                List {
+                    ForEach(viewModel.showStories) { item in
+                        NavigationLink {
+                            PostScreen(item: item)
                         } label: {
-                            Label("Hide Viewed", systemImage: "eye")
+                            PostListItem(item: item)
                         }
                     }
                 }
-                .navigationTitle(Text("Show Stories"))
             }
+        }
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    // TODO: Implement Hide Viewed Posts
+                } label: {
+                    Label("Hide Viewed", systemImage: "eye")
+                }
+            }
+        }
+        .navigationTitle(Text("Show Stories"))
     }
 }
+
